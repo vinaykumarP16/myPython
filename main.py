de2a9c276d5c3f278ead0efd6b8e68dd8de68d23
vinaykumarp16 <vinaykumar.p1995v@gmail.com>
2022-02-07 20:30:34 +0530

    add files

---
+++ main.py
@@ -0,0 +1,15 @@
+brand='vinaykumar p';
+age=2;
+number=[1,2,3,4]
+pi=1.222
+isAdult=True
+print(type(brand),type(age),type(number),type(pi),type(isAdult));
+
+vin=10;
+vin="vinay";
+print(type(vin));
+
+#im comment
+
+'''
+hello ehere'''
